1acff2442810541f6a18f3b2919e299751ba1323
William Waites <ww@groovy.net>
2018-10-31 14:30:44 +0000

    add minimal readme

---
+++ README.md
@@ -0,0 +1,7 @@
+Prune a large Bibtex database. extract only those entries necessary for
+a particular document from the .aux file.
+
+Example:
+
+    % bibtexprune chapter.aux thesis.bib chapter.bib
+
--- setup.py
+++ setup.py
@@ -3,6 +3,19 @@ from setuptools import setup
 setup(
     name='bibtexprune',
     version='1.0',
+    description="""\
+Prune a large Bibtex database. extract only those entries necessary for
+a particular document from the .aux file""",
+    author='William Waites',
+    url='https://github.com/wwaites/bibtexprune',
+    classifiers = [
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: Text Processing :: Markup :: LaTeX'
+    ],
     packages=['bibtexprune'],
     install_requires=['bibtexparser'],
     entry_points={
